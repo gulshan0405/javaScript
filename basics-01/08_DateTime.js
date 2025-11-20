083232822f39392ac87b4d547c47475efa342708
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-11-20 14:44:24 +0530

    Date and time

---
+++ basics-01/08_DateTime.js
@@ -0,0 +1,35 @@
+//Date
+
+let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof(mydate));
+
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// let myCreatedDate = new Date(2023, 0, 23,5,3);
+// let myCreatedDate = new Date("2023-01-14");
+let myCreatedDate = new Date("01-14-2023");
+
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp =Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date();
+// console.log(newDate);
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth());
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+   
+})
+
+
+
+
