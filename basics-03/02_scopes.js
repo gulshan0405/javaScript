82e855ed7702ab5f125b2fffca808c006a994a35
gulshan0405 <167597734+gulshan0405@users.noreply.github.com>
2025-11-21 15:27:58 +0530

    Hoisting in JS

--- basics-03/02_scopes.js
+++ basics-03/02_scopes.js
@@ -6,13 +6,50 @@ if (true) {
   let a = 10; //local scope
   const b = 20; //local scope
   var c = 30; //global scope
-  console.log("INNER: ",a)
+  //   console.log("INNER: ",a)
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-}
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+  const username = "hitesh";
+  function two() {
+    const website = "youtube";
+    // console.log(username);
+  }
+  // console.log(website);
+  two();
+}
+
+one();
+
+if (true) {
+  const username = "hitesh";
+  if (username === "hitesh") {
+    const website = "youtube";
+    // console.log(username+website)
+  }
+  // console.log(website);
+}
+// console.log(username)
+
+//---------------Interesting -----------------
+//here we are calling the function before initializing addone is the normalfunction
+//but addtwo is function which is stored in the variable 
+//addone can able to access but addtwo can't able to access this 
+//phenomena in js is called as hoisting
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+
+// console.log(addTwo(6));
+const addTwo = function (num) {
+  return num + 2;
+};
+
+
